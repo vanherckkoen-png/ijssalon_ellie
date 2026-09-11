0aad9e38dbc2695ccb479cdb53f6000911ab1952
Koen Vanherck <vanherck.koen@telenet.be>
2026-09-11 03:23:31 +0200

    bestand tijdelijk gecodeerd

--- helper.py
+++ helper.py
@@ -5,3 +5,12 @@ def decoreer(tekst=""):
     print(f"* {tekst} *")
     print (lengte*"*")
     print()
+def fooi_pp(bedrag, personen):
+    try:
+        bedrag_pp = bedrag/personen
+    except:
+        bedrag_pp = "??"
+    return(f"bedrag per persoon is {bedrag_pp} €")
+bedrag = int(input("bedrag fooienpot ? "))
+personen = int(input("aantal personen ? "))
+print(fooi_pp(bedrag, personen))
